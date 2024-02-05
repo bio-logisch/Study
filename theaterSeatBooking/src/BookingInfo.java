1409056a4db71cbb0c6ae7b1f910ffcda5c14e88
bio-logisch <153245050+bio-logisch@users.noreply.github.com>
2024-02-05 14:38:34 +0900

    Update and rename src/BookingInfo.java to theaterSeatBooking/src/BookingInfo.java

---
+++ theaterSeatBooking/src/BookingInfo.java
@@ -0,0 +1,15 @@
+package theater;
+
+public class BookingInfo {
+	
+	String userName=null; ///예매자 이름
+	String userPass=null; ///예매자 비밀번호
+	int row = 0;//행번호
+	int col = 0;//열번호
+	
+/*	public void maskingPrt() { 여기서 이렇게 만들어도 된다. 참고용으로만 보기
+		
+		
+	}*/
+
+}

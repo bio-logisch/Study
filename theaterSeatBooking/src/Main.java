326fad1fda8f365b542a2e0e9538c9ab47f52a53
bio-logisch <153245050+bio-logisch@users.noreply.github.com>
2024-02-14 08:49:36 +0900

    Update Main.java

--- theaterSeatBooking/src/Main.java
+++ theaterSeatBooking/src/Main.java
@@ -2,10 +2,8 @@ package theater;
 
 public class Main {
 	
-	public static void main(String[] args) { ///메인메서드에는 반드시 static이 있어야한다.
-		new MainMenu(); ///객체를 만들 땐 생성자로 만든다. 생성자도 메서드이다.
-	//	SeatMain s = new SeatMain();
-	//	s.menu();
+	public static void main(String[] args) {
+		new MainMenu(); 
 	}
 
 }

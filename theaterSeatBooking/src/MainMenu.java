8f7df37b38feafb5e877e4a8a64ddaf45db913dc
bio-logisch <153245050+bio-logisch@users.noreply.github.com>
2024-02-14 08:49:17 +0900

    Update MainMenu.java

--- theaterSeatBooking/src/MainMenu.java
+++ theaterSeatBooking/src/MainMenu.java
@@ -6,8 +6,8 @@ public class MainMenu {
 //	MemberMain memberMain = new MemberMain(); ///고객관리 객체를 MainMenu가 만든 것임, 싱글톤으로 바꾸기 위해 주석처리
 	MemberMain memberMain = MemberMain.getInstance(); //싱글톤을 위해 해당 객체 새로 생성
 	
-	SeatMain seatMain = new SeatMain(); ///예약 객체를 MainMenu가 만든 것임
-	Member nowUser = null; ///null이니까 로그인 한 사람이 없다는 의미임(로그인 한 사람 정보)
+	SeatMain seatMain = new SeatMain(); 
+	Member nowUser = null; //로그인 한 사람 정보
 	MainMenu() { 
 		// 메뉴선택하기.. ///생성자에 메뉴기능을 정의함. 객체를 생성하자마자(프로그램 실행하자마자) 메뉴를 띄운다는게 의도
 		boolean f = true;

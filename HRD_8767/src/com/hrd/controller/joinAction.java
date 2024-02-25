58a71790e55f9740f2968ec69838132b79159d28
bio-logisch <153245050+bio-logisch@users.noreply.github.com>
2024-02-25 15:58:32 +0900

    Update joinAction.java

--- HRD_8767/src/com/hrd/controller/joinAction.java
+++ HRD_8767/src/com/hrd/controller/joinAction.java
@@ -29,12 +29,16 @@ public class joinAction extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//System.out.println("컨트롤러 작동");//확인용
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		// 파라미터 받기
-		//String custno = (String)request.getParameter("name");  *디비쿼리에서 생성하여 생략
 		int custno=Integer.parseInt(request.getParameter("custno"));
 		String custname = (String)request.getParameter("custname");
 		String phone = (String)request.getParameter("phone");
@@ -42,32 +46,23 @@ public class joinAction extends HttpServlet {
 		String joindate = (String)request.getParameter("joindate");
 		String grade = (String)request.getParameter("grade");
 		String city = (String)request.getParameter("city");
+	    
+		System.out.println(custno +"/"+custname);//확인용
 		
-		System.out.println(custname +"/"+phone);//확인용
-		
-		// dao 에게 전달한 vo
-		// 번호자동 생성 이라는 기능을 구현한다면 자동생성된 번호도 추가 해야 함
-		MemberVO mvo = new MemberVO();
-		mvo.setCustno(custno);
-		mvo.setCustname(custname);
-		mvo.setCity(city);
-		mvo.setAddress(address);
-		mvo.setGrade(grade);
-		mvo.setJoindate(joindate);
-		mvo.setPhone(phone);
-		
-		MemberDAO mdao = MemberDAO.getInstance();
-		mdao.insert(mvo);
-	
-	
+		 // dao 에게 전달한 vo
+	  	  MemberVO mvo = new MemberVO();
+	  	  mvo.setCustno(custno);
+	   	 mvo.setCustname(custname);
+	   	 mvo.setCity(city);
+	   	 mvo.setAddress(address);
+	   	 mvo.setGrade(grade);
+	   	 mvo.setJoindate(joindate);
+	   	 mvo.setPhone(phone);
+	   	 MemberDAO mdao = MemberDAO.getInstance();
+		//System.out.println("db로 insert 시도");//확인용
+	   	 mdao.insert(mvo);
+		//System.out.println("db로 insert 완료");//확인용
+	    	// 등록 후에는 목록 페이지로 이동
+	   	 response.sendRedirect("listView"); //가입후 리스트로 돌아가기
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }

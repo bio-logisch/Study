eabeef95f20b114f179a4bef47ccab1dda26f3e5
bio-logisch <153245050+bio-logisch@users.noreply.github.com>
2024-02-25 16:00:19 +0900

    Update viewMoney.java

--- HRD_8767/src/com/hrd/controller/viewMoney.java
+++ HRD_8767/src/com/hrd/controller/viewMoney.java
@@ -31,25 +31,16 @@ public class viewMoney extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
-		
-		//2. 파라미터 받을 필요없다. 그러나 페이지기능이 있다면 몇번 페이지를 요청했는지 받아야 한다.
-		//3. 모델    쿼리 > 리턴받을 값 (DTO단일 객체이거나 여러개일 경우는 List많이 사용)
-		
-		MemberDAO mdao = MemberDAO.getInstance(); // 문제가 있냐면 계속 객체를 만들어요..>싱글톤(객체는 한개만 만들고 공유)
+		response.setContentType("text/html; charset=UTF-8");	
+		MemberDAO mdao = MemberDAO.getInstance(); // 싱글톤
 		ArrayList<ViewMoneyVO> listMember = new ArrayList<>();
 		listMember = mdao.selectTotalAll();
-		
-		// 디버깅하시오
 		System.out.println(listMember.size()+" 건 가져옴-매출내용 [디버깅]");
-		
-		// 4. 뷰지정
-		// view지정..
+		// 뷰지정
 		String url = "viewMoney.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		request.setAttribute("totalList", listMember);  // 타입.. 리스트 타입.. 
+		request.setAttribute("totalList", listMember); 
 		dispatcher.forward(request, response);
 	}
 
